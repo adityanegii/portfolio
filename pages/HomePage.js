448f27e6b5b9177a18ae83de2c5521df410629bf
Aditya Negi <aditya.negi@mail.mcgill.ca>
2022-09-17 12:26:03 -0400

    minor tweaks

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -3,11 +3,15 @@ import styles from '../styles/HomePage.module.css'
 export default function HomePage() {
   return (
     <div className={styles.container}>
-       <a style = {{fontSize: "3em"}}>Hey, I'm </a>
-       <span>Aditya Negi</span> 
+      <div className={styles.title}>
+        <a style = {{fontSize: "3em"}}>Hey, I'm </a>
+        <span>Aditya Negi</span> 
+      </div>
        <br/>
-       <a>I'm a software engineering student at McGill University</a>
-       <h1>I suck because I failed to get Damso billets</h1>
+      <div>
+        <a>I'm a software engineering student at McGill University</a>
+        <h1>I suck because I failed to get Damso billets</h1>
+      </div>
     </div>
   )
 }
