6afdd9142942168fd531ae539836ff7c38688ef4
Aditya Negi <aditya.negi@mail.mcgill.ca>
2022-09-22 01:01:28 -0400

    Added flipcards to experience page and adjusted them so they can generate content dynamically

--- components/assets/FlipCard.js
+++ components/assets/FlipCard.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import styles from '../../styles/Assets.module.css'
 
-export default function FlipCard() {
+export default function FlipCard({card}) {
   return (
     <div className={styles.flip_card}>
-  <div className={styles.flip_card_inner}>
-    <div className={styles.flip_card_front}>
+      <div className={styles.flip_card_inner}>
+        <div className={styles.flip_card_front}>
+          <p>{card.front1}</p>
+          <p>{card.front2}</p>
+        </div>
+        <div className={styles.flip_card_back}>
+          <p>{card.back1}</p>
+          <p>{card.back2}</p>
+        </div>
+      </div>
     </div>
-    <div className={styles.flip_card_back}>
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
-    </div>
-  </div>
-</div>
   )
 }
--- pages/Experience.js
+++ pages/Experience.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import FlipCard from '../components/assets/FlipCard';
+import styles from '../styles/Experience.module.css';
+const cards = [
+    {
+      id: "1",
+      front1: "Equifax Inc.",
+      front2: "Junior Software Engineer",
+      back1: "Assisted the development, and testing of attributes for the Attribute Engine using Python.",
+      back2: "Designed and developed a program to transform fixed format credit reports into CSV format to make them more accessible"
+    },
+    {
+      id: "2",
+      front1: "CHAP McGill",
+      front2: "Co-founder, Developper",
+      back1: "At CHAP our goal is to help students gain experience in software development. We do this by creating projects, and participating in hackathos.",
+    }
+  ];
 
 const  Experience = () => {
     return ( 
-        <div>Experience</div>        
+        <div className={styles.container}>
+            {cards.map((card) => (
+            <FlipCard key={card.id} card={card} />
+          ))}
+        </div>        
      );
 }
  
