5f4a2b00e2e3a1344296f3eeb4c89d192258cab6
adityanegii <aditya.negi@mail.mcgill.ca>
2023-10-07 00:04:59 -0400

    update

--- pages/Experience.js
+++ pages/Experience.js
@@ -8,7 +8,7 @@ const cards = [
       front1: "THORASYS",
       front2: "Software Developer Intern",
       date: "July-August 2023",
-      back: "During my internship, I was actively engaged in the development of a proof of concept for a Windows service using Delphi. The primary objective of this service was to streamline the retrieval and analysis of data from one of THORASYS' products. Upon completing the analysis, the service had the capability to transmit the processed data to a third-party software via a TCP/IP connection. I am pleased to have successfully implemented it.",
+      back: "During my internship, I was actively engaged in the development of a proof of concept for a Windows service using Delphi. The primary objective of this service was to streamline the retrieval and analysis of data from one of THORASYS' products. Upon completing the analysis, the service had the capability to transmit the processed data to a third-party software via a TCP/IP connection.",
       photo: "thorasys.jfif"
     },
     {
